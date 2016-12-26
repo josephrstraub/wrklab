47c76926c5c6d187f2ae378a4e0184e9ab2423d3
josephrstraub <josephrstraub@gmail.com>
2016-12-26 13:58:28 -0500

    serve all routes

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.set('port', (process.env.PORT || 3001))
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
+  app.get('*/:page', (req, res) => {
+    res.sendFile(path.resolve('client/build', 'index.html'));
+  });
+  app.use(function(req, res) {
+    res.status(404).send('Sorry cant find that!');
+  });
 }
 
 mongoose.connect(process.env.MONGODB_URI || mongoLocal)
