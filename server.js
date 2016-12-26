cd22aa6d4411d1eccd75ed184a6b5b1b3d3d8efb
josephrstraub <josephrstraub@gmail.com>
2016-12-26 14:26:02 -0500

    serve all routes

--- server.js
+++ server.js
@@ -19,12 +19,7 @@ app.set('port', (process.env.PORT || 3001))
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
-  app.get('*/:page', (req, res) => {
-    let routes = ['featured', 'vision', 'process']
-    if ( (routes.includes(page) && !page.includes('api')) || !page ) {
-      res.sendFile(path.resolve('client/build', 'index.html'));
-    }
-  });
+  app.get('/featured', (req, res) => res.sendFile(path.resolve('client/build', 'index.html')));
   app.use(function(req, res) {
     res.status(404).send('Sorry cant find that!');
   });
